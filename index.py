f662b6d05a5ae6dd6861f024e4eaa0118ad554fd
huynslol <oliviervandhuynslager@gmail.com>
2021-02-06 17:40:29 +0100

    debug

--- index.py
+++ index.py
@@ -14,13 +14,14 @@ app.layout = html.Div([
     html.Div(id="page-content", children=[])
 ])
 
-@app.callback(Output("page-content", "children"),
-              Input("url", "pathname"))
+
+@app.callback(Output(component_id="page-content", component_property="children"),
+              Input(component_id="url", component_property="pathname"))
 def display_page(pathname):
     if pathname == "/apps/app1":
         return app1.layout
     else:
-        return "404"
+        return "404 page error!"
 
 
 if __name__ == '__main__':
